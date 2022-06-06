a85c5d5c26d4f7bef471a81ac707b3f59061b66f
Thomas Anciaux <1404536+thomaux@users.noreply.github.com>
2022-06-06 15:40:22 +0200

    Improve team screen: layout, FAB, back navigation

--- lib/src/widgets/team.dart
+++ lib/src/widgets/team.dart
@@ -13,33 +13,40 @@ class TeamWidget extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Beheer je team'),
-        actions: [
-          Builder(
-            builder: (context) => IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () => Scaffold.of(context).openEndDrawer(),
-              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-            ),
-          ),
-        ],
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        actions: [Container()], // Hide the drawer icon
       ),
       body: Consumer<TeamProvider>(
         builder: (context, db, child) {
           return FutureBuilder<List<Player>>(
             future: db.listPlayers(),
             builder: (innerContext, snapshot) {
-              if(snapshot.hasData) {
-                return ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: snapshot.data!.length,
-                itemBuilder: (BuildContext innerContext, int index) {
-                  return Text('' + snapshot.data![index].name);
-              });
+              if (snapshot.hasData) {
+                return ListView.separated(
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (BuildContext innerContext, int index) {
+                      return ListTile(
+                        title: Text(snapshot.data![index].name),
+                      );
+                    },
+                    separatorBuilder: (_, index) => const Divider(height: 1));
               }
               return const Text('Loading...');
             },
           );
         },
+      ),
+      floatingActionButton: Builder(
+        builder: (context) => FloatingActionButton(
+          onPressed: () => Scaffold.of(context).openEndDrawer(),
+          tooltip: 'Beheer team',
+          child: const Icon(Icons.add),
+        ),
       ),
       endDrawer: const Drawer(
         child: AddPlayerWidget(),
